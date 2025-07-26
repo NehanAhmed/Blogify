2eb03c7cdc4ce6e93565f630e470b3f8e6fea469
Nehan Ahmed <nehanahmed2k23@gmail.com>
2025-07-26 11:51:09 +0500

    completed auth and post appwrite configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Footer, Header } from './components'
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL) // This will log "Helllo" to the console
-  return (
-    <>
-      <h1>Hello world!</h1>
-    </>
-  )
+  
+  const [loading, setloading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setloading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen w-screen bg-zinc-800'>
+      <div className='w-full h-full'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+
+      </div>
+
+    </div>
+  ) : null
 }
 
 export default App
